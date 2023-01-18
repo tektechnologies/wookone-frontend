0857fce0ae63131de7d248a9fd06e00dc58b70c4
tektechnologies <info@tektechnologies.com>
2023-01-17 20:40:04 -0600

    format js file

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,7 +2,11 @@ import React from "react";
 
 class Footer extends React.Component {
   render() {
-    return <footer className="container-fluid text-center"><h2>Welcome to WookOne</h2></footer>;
+    return (
+      <footer className="container-fluid text-center">
+        <h2>Welcome to WookOne</h2>
+      </footer>
+    );
   }
 }
 
