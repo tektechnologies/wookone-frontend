805a0ef2658db884bb936b627da1eed312d8af2c
tektechnologies <info@tektechnologies.com>
2023-01-17 20:37:41 -0600

    add color padding to footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Footer extends React.Component {
   render() {
-    return <footer>wookOne</footer>;
+    return <footer className="container-fluid text-center"><h2>Welcome to WookOne</h2></footer>;
   }
 }
 
