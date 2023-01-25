0074c7e7723c213e852fa0f7a9040094a0b052fb
tektechnologies <info@tektechnologies.com>
2023-01-25 15:38:44 -0600

    add form submit() and loggin the api url

--- src/main/Main.js
+++ src/main/Main.js
@@ -11,20 +11,37 @@ import './mainCSS/main.css';
   }
 
    handleInput = (event) => {
-    // event.preventDefault();
       this.setState({
         citySearched: event.target.value,
       });
     }
+
+    searchCityAPI = async (event) => {
+      event.preventDefault();
+      
+      let searchCityURL = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.citySearched}&format=json`;
+      
+      console.log("🚀 ~ file: Main.js:24 ~ Main ~ searchCityAPI= ~ searchCityURL", searchCityURL);
+      // let cityData = await axios.get(searchCityURL);
+
+      // this.setState({
+
+      // });
+    }
+
+
+
+
   render() {
     console.log('city input in state', this.state.citySearched);
     return (
       <main>
-      <form>
+      <form onSubmit={this.searchCityAPI}>
         <label>
-          Search For City
+          Search For City: 
           <input type="text" onInput={this.handleInput}/>
         </label>
+        <button>Search</button>
       </form>
       </main>
     )
