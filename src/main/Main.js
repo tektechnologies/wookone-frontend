881419b3a5dafbc7beb0af7e834ec0a2b6604a84
tektechnologies <info@tektechnologies.com>
2023-01-25 15:22:21 -0600

    add form stateand onInput()

--- src/main/Main.js
+++ src/main/Main.js
@@ -3,10 +3,28 @@ import './mainCSS/main.css';
 
 
  class Main extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      citySearched: "",
+    };
+  }
+
+   handleInput = (event) => {
+      this.setState = {
+        citySearched: event.target.value,
+      }
+    }
   render() {
+   
     return (
       <main>
-      wookOne
+      <form>
+        <label>
+          Search For City
+          <input type="text" onInput={this.handleInput}/>
+        </label>
+      </form>
       </main>
     )
   }
