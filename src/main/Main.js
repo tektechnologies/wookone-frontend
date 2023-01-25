24d3d3b38cf1a62fe76cb899e8d969781aa47f55
tektechnologies <info@tektechnologies.com>
2023-01-25 15:48:55 -0600

    format js file

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import './mainCSS/main.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import "./mainCSS/main.css";
+import axios from "axios";
 
- class Main extends Component {
-  constructor(props){
+class Main extends Component {
+  constructor(props) {
     super(props);
     this.state = {
       citySearched: "",
@@ -11,44 +11,34 @@ import axios from 'axios';
     };
   }
 
-   handleInput = (event) => {
-      this.setState({
-        citySearched: event.target.value,
-      });
-    }
+  handleInput = (event) => {
+    this.setState({
+      citySearched: event.target.value,
+    });
+  };
 
-    searchCityAPI = async (event) => {
-      event.preventDefault();
-      
-      let searchCityURL = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.citySearched}&format=json`;
-      
-      let cityData = await axios.get(searchCityURL);
-  
-
-      this.setState({
-        cityData: cityData.data[0],
-      });
-    }
-
-
-
+  searchCityAPI = async (event) => {
+    event.preventDefault();
+    let searchCityURL = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.citySearched}&format=json`;
+    let cityData = await axios.get(searchCityURL);
+    this.setState({
+      cityData: cityData.data[0],
+    });
+  };
 
   render() {
-    console.log('city input in state', this.state.cityData);
     return (
       <main>
-      <form onSubmit={this.searchCityAPI}>
-        <label>
-          Search For City: 
-          <input type="text" onInput={this.handleInput}/>
-        </label>
-        <button>Search</button>
-      </form>
+        <form onSubmit={this.searchCityAPI}>
+          <label>
+            Search For City:
+            <input type="text" onInput={this.handleInput} />
+          </label>
+          <button>Search</button>
+        </form>
       </main>
-    )
+    );
   }
 }
-   
-
 
 export default Main;
