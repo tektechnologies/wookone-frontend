ab55b97022e62d9d0b7fe15899f246aa557b4c54
tektechnologies <info@tektechnologies.com>
2023-03-06 19:14:08 -0600

    add Home component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import "./App.css";
-import "./footer/footerCSS/footer.css";
-import Footer from "./footer/Footer";
-import Main from "./main/Main.js";
 import Header from "./header/Header.js";
+import Home from './home/Home.js';
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Header />}>
+      <Route index element={<Home />} />
+      {/* <Route path="profile" element={<Profile />} />
+      <Route path="login" element={<Login />} />
+      <Route path="logout" element={<Logout />} /> */}
+      {/* <Route path="help" element={<HelpComponent />}>
+        <Route path="faq" element={<FAQ />} />
+      </Route> */}
+    </Route>
+  )
+);
 function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <RouterProvider router={router} />
     </>
   );
 }
